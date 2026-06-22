d2e9284f59cfc04253098c5e7b84882bf80b46f6
rynkitty <rynsterbrook@gmail.com>
2026-06-22 20:11:26 +0100

    mech scriptable objects changed

--- Assets/Scripts/Mech Scripts/MechData.cs
+++ Assets/Scripts/Mech Scripts/MechData.cs
@@ -8,24 +8,37 @@ public class MechData : ScriptableObject
     public string pilotName;
 
     [Header("Stat")]
-    [Range(0, 10)]public int agilityStat;
-    [Range(0, 10)]public int strengthStat;
-    [Range(0, 4)]public int systemsStat;
-    [Range(0, 10)]public int reliabilityStat;
-    [Range(1, 3)] public int size = 1;
+    [Range(0, 10)] public int agilityStat;
+    [Range(0, 10)] public int strengthStat;
+    [Range(0, 4)]  public int systemsStat;
+    [Range(0, 10)] public int reliabilityStat;
+    [Range(1, 3)]  public int size = 1;
 
-    // Converts all stats to floats, multiple by value, then floor to int to get cost of mech
-    public int cost { get { return Mathf.FloorToInt(((float)agilityStat + (float)strengthStat + (float)systemsStat) * reliabilityStat); } }
+    // (agility + strength + systems) * reliability, floored
+    public int cost { get { return Mathf.FloorToInt(((float)agilityStat + strengthStat + systemsStat) * reliabilityStat); } }
 
-    // Constructor
-    public MechData(string newName, string newPilot, int newAgility, int newStrength, int newSystems, int newReliability, int newSize)
+    public static MechData Create(string name, string pilot, int agility, int strength, int systems, int reliability, int size)
     {
-        this.mechName = newName;
-        this.pilotName = newPilot;
-        this.agilityStat = newAgility;
-        this.strengthStat = newStrength;
-        this.systemsStat = newSystems;
-        this.reliabilityStat = newReliability;
-        this.size = newSize;
+        var data = CreateInstance<MechData>();
+        data.mechName = name;
+        data.pilotName = pilot;
+        data.agilityStat = agility;
+        data.strengthStat = strength;
+        data.systemsStat = systems;
+        data.reliabilityStat = reliability;
+        data.size = size;
+        return data;
     }
 }
+
+#if UNITY_EDITOR
+[UnityEditor.CustomEditor(typeof(MechData))]
+public class MechDataEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        UnityEditor.EditorGUILayout.LabelField("Cost", ((MechData)target).cost.ToString());
+    }
+}
+#endif
